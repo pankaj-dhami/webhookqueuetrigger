242e6e04aeecac4b665c38a9a9065007a1a90a36
pankaj dhami <pankaj.rocksmile@hotmail.com>
2017-11-18 19:50:49 +0530

    Add project files.

---
+++ AzureQueueTriggerWebjob/BackgroundJobs.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.WindowsAzure.Storage.Queue;
+using System;
+
+namespace AzureQueueTriggerWebjob
+{
+    public class BackgroundJobs
+    {
+        public static void TriggerFunction([QueueTrigger("pdqueue")]CloudQueueMessage message)
+        {
+            var queuemessge = message.AsString;
+            System.Console.WriteLine(queuemessge);
+        }
+        public static void FiveSecondTask([TimerTrigger("* */1  */0 * * *")] TimerInfo timer)
+        {
+            Console.WriteLine("This should run every 5 seconds");
+        }
+
+        //  ┌───────────── minute(0 - 59)
+        //# │ ┌───────────── hour (0 - 23)
+        //# │ │ ┌───────────── day of month (1 - 31)
+        //# │ │ │ ┌───────────── month (1 - 12)
+        //# │ │ │ │ ┌───────────── day of week (0 - 6) (Sunday to Saturday;
+        //# │ │ │ │ │                                       7 is also Sunday on some systems)
+        //# │ │ │ │ │
+        //# │ │ │ │ │
+        //# * * * * *  command to execute
+    }
+}
---
+++ AzureQueueTriggerWebjob/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.WindowsAzure.Storage.Queue;
+using System;
+using System.Configuration;
+
+namespace AzureQueueTriggerWebjob
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var _storageConn = ConfigurationManager
+           .ConnectionStrings["AzureWebJobsStorage"].ConnectionString;
+
+            var _dashboardConn = ConfigurationManager
+           .ConnectionStrings["AzureWebJobsDashboard"].ConnectionString;
+
+            JobHostConfiguration config = new JobHostConfiguration();
+            config.StorageConnectionString = _storageConn;
+            //config.DashboardConnectionString = _dashboardConn;
+            config.UseTimers();
+            config.Queues.MaxDequeueCount = 2;
+            config.Queues.MaxPollingInterval = TimeSpan.FromSeconds(4);
+            config.Queues.BatchSize = 2;
+
+            JobHost host = new JobHost(config);
+            host.RunAndBlock();
+        }
+
+    }
+}
